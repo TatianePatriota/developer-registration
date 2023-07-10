f2301d8bc3cef7bb12f04096afda68ce827143cb
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-07-10 16:09:19 -0300

    add header  component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { StyledContainer } from "./StyledHeader";
+
+export function Header() {
+  return (
+    <StyledContainer>
+      <h1>Cadastro de Desenvolvedores</h1>
+      <p>Faço seu Cadastro na lista para receber ofertas de empregos</p>
+    </StyledContainer>
+  );
+}
