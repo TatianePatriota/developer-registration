2bf9ea42d850170997bd09d7821c75290c3c1f9e
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-07-17 09:54:28 -0300

    add component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./components/router.tsx";
+import { FormProvider } from "./contexts/FormContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <FormProvider>
+      <RouterProvider router={router} />
+    </FormProvider>
   </React.StrictMode>
 );
