1db8b3e190ecae89473c2ffd0ec38e7db29db98d
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-07-19 14:27:45 -0300

    add global style

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,23 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./components/router.tsx";
 import { FormProvider } from "./contexts/FormContext.tsx";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import { theme } from "../themes";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <FormProvider>
-      <RouterProvider router={router} />
-    </FormProvider>
+    <ThemeProvider theme={theme}>
+      <FormProvider>
+        <RouterProvider router={router} />
+      </FormProvider>
+    </ThemeProvider>
+    <GlobalStyle />
   </React.StrictMode>
 );
