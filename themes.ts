2f6f0a51103ec9a85652d2ab7e1b09423480701e
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-07-19 14:28:26 -0300

    add theme

---
+++ themes.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    darkBluePrimary: "#16195c",
+    darkBlueSecondary: "#02044a",
+    lightBlue: "#b8b8d4",
+    white: "#FFFFFF",
+    green: "#25cd89",
+  },
+};
