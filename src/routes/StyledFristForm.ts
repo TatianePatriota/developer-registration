16cb369cec9c26fe43bee3c283e6628b21781ac3
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-07-20 09:40:00 -0300

    add new styles and props colors

--- src/routes/StyledFristForm.ts
+++ src/routes/StyledFristForm.ts
@@ -1,17 +1,23 @@
 import { styled } from "styled-components";
 
-export const StyledContainerFristForm = styled.div`
+export const StyledContainer = styled.div`
   & h3 {
     font-size: 13px;
-    color: #b8b8d4;
+    color: ${({ theme }) => theme.colors.lightBlue};
     padding-bottom: 30px;
-    border-bottom: 1px solid #16195c;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.darkBluePrimary};
   }
 
   & h1 {
     margin: 0;
     padding: 0;
     font-size: 26px;
+  }
+
+  & h5 {
+    display: none;
+    color: red;
+    margin: 5px 0;
   }
 
   & label {
@@ -24,23 +30,34 @@ export const StyledContainerFristForm = styled.div`
     box-sizing: border-box;
     width: 100%;
     padding: 20px 10px;
-    border: 2px solid #25cd89;
+    border: 2px solid ${({ theme }) => theme.colors.green};
     border-radius: 10px;
-    color: #ffffff;
+    color: ${({ theme }) => theme.colors.white};
     font-size: 15px;
-    background-color: #02044a;
+    background-color: ${({ theme }) => theme.colors.darkBlueSecondary};
     outline: none;
   }
 
   & button {
-    background-color: #25cd89;
+    background-color: ${({ theme }) => theme.colors.green};
     border: none;
     font-weight: 700;
     font-size: 14px;
     padding: 20px 40px;
     border-radius: 25px;
-    color: #ffffff;
+    color: ${({ theme }) => theme.colors.white};
     cursor: pointer;
-    margin-top: 30px;
+    margin-top: 20px;
+  }
+
+  & .backButton {
+    font-size: 16px;
+    text-decoration: none;
+    padding: 20px 40px;
+    color: ${({ theme }) => theme.colors.black};
+    background-color: ${({ theme }) => theme.colors.lightGrey};
+    border-radius: 25px;
+    font-weight: 700;
+    margin-right: 30px;
   }
 `;
