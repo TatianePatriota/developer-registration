5a1ca4f0789029793b79ac718b93de384a229b82
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-07-24 09:47:41 -0300

    add props active

--- src/components/ThemeComponent.tsx
+++ src/components/ThemeComponent.tsx
@@ -8,12 +8,15 @@ import {
 } from "./StyledTheme";
 import { Header } from "./Header";
 import { IconsName, SidebarItem } from "./SidebarItem";
+import { useForm } from "../contexts/FormContext";
 
 type ThemeProps = {
   children: ReactNode;
 };
 
 export function ThemeComponent({ children }: ThemeProps) {
+  const { state } = useForm();
+
   return (
     <StyledContainer>
       <StyledArea>
@@ -25,18 +28,21 @@ export function ThemeComponent({ children }: ThemeProps) {
               description="Se identifique"
               iconName={IconsName.Profile}
               path="/"
+              active={state.currentStep === 1}
             />
             <SidebarItem
               title="Profissional"
               description="Se nível"
               iconName={IconsName.Book}
               path="/step2"
+              active={state.currentStep === 0}
             />
             <SidebarItem
               title="Contatos"
               description="Como te achar"
               iconName={IconsName.Email}
               path="/step3"
+              active={state.currentStep === 3}
             />
           </StyledSidebar>
           <StyledPage>{children}</StyledPage>
